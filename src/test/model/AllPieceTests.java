9cce78d874d8b6650028e1d115483277de315b4f
rafael <reservasegunda7@gmail.com>
2025-05-16 14:11:36 -0300

    Update AllPieceTests.java
    adicionei QueenTest e BishopTest

--- src/test/model/AllPieceTests.java
+++ src/test/model/AllPieceTests.java
@@ -6,7 +6,9 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     KnightTest.class,
-	RookTest.class,
-    PawnTest.class
+    RookTest.class,
+    PawnTest.class,
+    BishopTest.class,
+    QueenTest.class 
 })
 public class AllPieceTests {}
