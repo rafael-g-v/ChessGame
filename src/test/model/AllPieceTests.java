8ce416a74fa122b9c5ac0f98af0d558123654fd0
Dante <dantenavaza2005@gmail.com>
2025-05-15 21:22:19 -0300

    pawn test

---
+++ src/test/model/AllPieceTests.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    //BishopTest.class,
+    //KnightTest.class,
+	RookTest.class,
+    PawnTest.class
+})
+public class AllPieceTests {}
---
+++ src/test/model/PawnTest.java
@@ -0,0 +1,37 @@
+package model;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PawnTest {
+
+    private Board board;
+    private Pawn whitePawn;
+    private Pawn blackPawn;
+
+    @Before
+    public void setUp() {
+        board = new Board();
+        whitePawn = new Pawn(true);
+        blackPawn = new Pawn(false);
+    }
+
+    @Test(timeout = 2000)
+    public void validPawnTestMoveOneHouseFront() 
+    {
+        Position from = new Position(6, 3); // posição inicial do peão branco
+        Position to = new Position(5, 3);   // avançar uma casa para frente (válido)
+
+        assertTrue("Peão branco deve avançar uma casa para frente", whitePawn.isValidMove(from, to, board));
+    }
+
+    @Test(timeout = 2000)
+    public void invalidPawnTestMoveOneHouseBack()
+    {
+        Position from = new Position(6, 3); // posição inicial do peão branco
+        Position to = new Position(7, 3);   // tentar mover para trás, inválido
+
+        assertFalse("Peão não pode mover para trás", whitePawn.isValidMove(from, to, board));
+    }
+}
