070b7a8ad9067be17ecfe13d9b4c332f7f09277b
Dante <dantenavaza2005@gmail.com>
2025-05-16 21:43:30 -0300

    Update KnightTest.java

--- src/test/model/KnightTest.java
+++ src/test/model/KnightTest.java
@@ -7,13 +7,11 @@ import org.junit.Test;
 public class KnightTest {
 
     private Board board_empty;
-    private Board board_filled;
     private Knight whiteKnight;
 
     @Before
     public void prepare() {
         board_empty = new Board(true);
-        board_filled = new Board(false);
         whiteKnight = new Knight(true);
     }
 
@@ -31,29 +29,5 @@ public class KnightTest {
         Position to = new Position(0, 3);   // movimento reto inválido para cavalo
 
         assertFalse("Cavalo não pode mover-se em linha reta", whiteKnight.isValidMove(from, to, board_empty));
-    }
-
-    @Test(timeout = 2000)
-    public void validKnightTestCaptureOpponent() {
-        Position from = new Position(0, 1);
-        Position to = new Position(2, 2);
-
-        // Caso a posição 2,2 está ocupada por peça adversária
-        Piece opponentPawn = new Pawn(false); // peça preta inimiga
-        board_filled.movePiece(2, 2, opponentPawn);
-
-        assertTrue("Cavalo pode capturar peça adversária", whiteKnight.isValidMove(from, to, board_filled));
-    }
-
-    @Test(timeout = 2000)
-    public void invalidKnightTestCaptureOwnPiece() {
-        Position from = new Position(0, 1);
-        Position to = new Position(2, 2);
-
-        // Caso a posição 2,2 tenha peça aliada
-        Piece ownPawn = new Pawn(true); 
-        board_filled.movePiece(2, 2, ownPawn);
-
-        assertFalse("Cavalo não pode capturar peça da mesma cor", whiteKnight.isValidMove(from, to, board_filled));
     }
 }
