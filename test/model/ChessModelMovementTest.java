faa82ef110fdaa2fcaf353d0a5544c25f73bcc76
BrenoSI03 <bresoa0@gmail.com>
2025-05-18 00:49:29 -0300

    Adicionando o cheque descoberto

--- test/model/ChessModelMovementTest.java
+++ test/model/ChessModelMovementTest.java
@@ -49,4 +49,40 @@ public class ChessModelMovementTest {
 
         assertFalse(model.isInCheck(true));
     }
+    
+    @Test
+    public void testMovimentoColocaReiPretoEmCheque() {
+        Board board = new Board(true);
+        board.clear();  // limpar o tabuleiro
+
+        // Colocar rei preto em e8 (0,4)
+        board.setPiece(0, 4, new King(false));
+        // Colocar dama branca em d1 (7,3)
+        board.setPiece(7, 3, new Queen(true));
+
+        ChessModel model = ChessModel.getInstance();
+        model.setBoard(board);
+
+        // Seleciona a dama branca e move para e2 (6,4), alinhada com o rei preto
+        model.selecionaPeca(7, 3);
+        model.selecionaCasa(6, 4);
+
+        // Verifica que o rei preto está em cheque
+        assertTrue(model.isInCheck(false));
+        
+        // Seleciona a rei preto e move para f8 (0,5)
+        model.selecionaPeca(0, 4);
+        model.selecionaCasa(0, 5);
+
+        // Verifica que o rei preto saiu do cheque
+        assertFalse(model.isInCheck(false));
+        
+        // Seleciona a dama branca e move para f3 (5,5), alinhada com o rei preto
+        model.selecionaPeca(6, 4);
+        model.selecionaCasa(5, 5);
+
+        // Verifica que o rei preto está em cheque
+        assertTrue(model.isInCheck(false));
+    }
+
 }
