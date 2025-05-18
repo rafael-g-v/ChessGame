5f7fa70e3e410c46e6c1438f94698092446411c1
rafael <reservasegunda7@gmail.com>
2025-05-18 15:09:22 -0300

    Update Position.java

--- src/model/Position.java
+++ src/model/Position.java
@@ -1,15 +1,19 @@
 package model;
 
 class Position {
-    
     public final int row;
     public final int col;
-
+    
     public Position(int row, int col) {
         this.row = row;
         this.col = col;
     }
 
+    /**
+     * @param obj Objeto a ser comparado
+     * @return true se as posições têm as mesmas coordenadas
+     */
+    @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof Position)) return false;
         Position other = (Position) obj;
