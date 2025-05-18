737ada77100bfc6325bae1e07ece0ac4f71b9d7e
rafael <reservasegunda7@gmail.com>
2025-05-18 15:07:49 -0300

    Update Piece.java

--- src/model/Piece.java
+++ src/model/Piece.java
@@ -3,13 +3,23 @@ package model;
 abstract class Piece {
     protected boolean isWhite;
 
+    /**
+     * Construtor base para todas as peças.
+     * @param isWhite true para peça branca, false para preta
+     */
     public Piece(boolean isWhite) {
         this.isWhite = isWhite;
     }
-
+    
     public boolean isWhite() {
         return isWhite;
     }
 
+    /**
+     * @param from Posição de origem
+     * @param to Posição de destino
+     * @param board Tabuleiro atual
+     * @return true se o movimento é válido para a peça
+     */
     public abstract boolean isValidMove(Position from, Position to, Board board);
 }
