fec242a9881af2553bc151bb80dc1e5ef028f5ee
Dante <dantenavaza2005@gmail.com>
2025-05-31 12:56:37 -0300

    mudancas

--- 2321406-2320363-2320469/src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import view.ConsoleView;
-
-public class Main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> new ConsoleView());
-    }
-}
