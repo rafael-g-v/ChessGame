a0350bb78937cbf5b317c0e92f840c0627673047
Dante <dantenavaza2005@gmail.com>
2025-06-13 17:08:37 -0300

    Update Main.java

--- 2321406-2320363-2320469/src/main/Main.java
+++ 2321406-2320363-2320469/src/main/Main.java
@@ -1,10 +1,9 @@
 package main;
 
-import view.ConsoleView;
+import view.StartView;
 
 public class Main {
-    public static void main(String[] args) 
-    {
-        new ConsoleView();
+    public static void main(String[] args) {
+        new StartView();  // Inicia pela tela inicial
     }
 }
