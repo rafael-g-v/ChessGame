c175a7407f22d9d8dccc0d9c4b1fde8558237e2d
Dante <dantenavaza2005@gmail.com>
2025-06-17 09:51:43 -0300

    mudancas

--- 2321406-2320363-2320469/src/model/ChessModel.java
+++ 2321406-2320363-2320469/src/model/ChessModel.java
@@ -603,9 +603,3 @@ public class ChessModel implements Observable  {
         this.showPromotionMenuRequested = false;
     }
 }
-/*
- * Observado: ter funcoes para gerenciar a lista de remover e adiconar da lista de eventos
- * Chamar um metodo no facade de adicionar observador
- * Observador vai notificar o sistema de eventos
- * Criar arquivos de observador e observado talvez ate em um pacote deles tudo publico
- */
